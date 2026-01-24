344d9893925808aa40e713609c350d24b19f1aa5
spaghetti-lover <phungducanh2511@gmail.com>
2026-01-24 21:27:21 +0700

    chore: refactor b+ tree index

---
+++ internal/storage/index/internal_node.go
@@ -0,0 +1,59 @@
+package index
+
+import "github.com/spaghetti-lover/go-db/internal/utils"
+
+type BPlusTreeInternalNode struct {
+	nkey     int
+	keys     [MAX_KEYS]int
+	children [MAX_CHILDREN]BPlusTreeNode
+}
+
+func NewBPlusTreeInternalNode() *BPlusTreeInternalNode {
+	return &BPlusTreeInternalNode{}
+}
+
+func (n *BPlusTreeInternalNode) isFull() bool {
+	return n.nkey >= MAX_KEYS
+}
+
+func (n *BPlusTreeInternalNode) InsertKV(key int, child BPlusTreeNode) {
+	idx := utils.FindLastLE(n.keys[:], n.nkey, key)
+
+	if idx != -1 && n.keys[idx] == key {
+		n.children[idx+1] = child
+		return
+	}
+
+	if n.isFull() {
+		panic("InsertKV called on full internal node")
+	}
+
+	for i := n.nkey; i > idx+1; i-- {
+		n.keys[i] = n.keys[i-1]
+		n.children[i+1] = n.children[i]
+	}
+
+	n.children[idx+2] = child
+	n.keys[idx+1] = key
+	n.nkey++
+}
+
+func (n *BPlusTreeInternalNode) Split() (int, *BPlusTreeInternalNode) {
+	mid := n.nkey / 2
+	promotedKey := n.keys[mid]
+
+	right := NewBPlusTreeInternalNode()
+
+	for i := mid + 1; i < n.nkey; i++ {
+		right.keys[i-mid-1] = n.keys[i]
+	}
+
+	for i := mid + 1; i <= n.nkey; i++ {
+		right.children[i-mid-1] = n.children[i]
+	}
+
+	right.nkey = n.nkey - mid - 1
+	n.nkey = mid
+
+	return promotedKey, right
+}
---
+++ internal/storage/index/leaf_node.go
@@ -0,0 +1,57 @@
+package index
+
+import "github.com/spaghetti-lover/go-db/internal/utils"
+
+type BPlusTreeLeafNode struct {
+	nkey   int
+	keys   [MAX_KEYS]int
+	values [MAX_KEYS]int
+}
+
+func NewBPlusTreeLeafNode() *BPlusTreeLeafNode {
+	return &BPlusTreeLeafNode{}
+}
+
+func (n *BPlusTreeLeafNode) isFull() bool {
+	return n.nkey >= MAX_KEYS
+}
+
+func (n *BPlusTreeLeafNode) InsertKV(key int, val int) {
+	idx := utils.FindLastLE(n.keys[:], n.nkey, key)
+
+	// Update existing key
+	if idx != -1 && key == n.keys[idx] {
+		n.values[idx] = val
+		return
+	}
+
+	if n.isFull() {
+		panic("InsertKV called on full leaf node")
+	}
+
+	// Shift and insert
+	if n.nkey < MAX_KEYS {
+		for i := n.nkey; i > idx+1; i-- {
+			n.keys[i] = n.keys[i-1]
+			n.values[i] = n.values[i-1]
+		}
+
+		n.values[idx+1] = val
+		n.keys[idx+1] = key
+		n.nkey++
+	}
+}
+
+func (n *BPlusTreeLeafNode) Split() *BPlusTreeLeafNode {
+	mid := n.nkey / 2
+	right := NewBPlusTreeLeafNode()
+
+	for i := mid; i < n.nkey; i++ {
+		right.keys[i-mid] = n.keys[i]
+		right.values[i-mid] = n.values[i]
+	}
+
+	right.nkey = n.nkey - mid
+	n.nkey = mid
+	return right
+}
